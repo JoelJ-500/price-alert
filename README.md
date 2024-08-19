703d369a21149fba8d94ae4d412444af19682ba6
Joel Jacob <joeljacob500@gmail.com>
2024-08-19 13:17:05 -0400

    edits planning doc to include time intervals and its price history conflicts

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # price-alert
+
+Retrieve the price of given Amazon product
+Store it in a database
+Run it on a server so it is periodically:
+ - Always checking prices/doing step 1
+ - Storing updated price in database
+ - Alerts user upon any price change
+
---
+++ server/main.py
@@ -0,0 +1,8 @@
+"""
+Simple prototype algorithim:
+Retrieves the price to the following amazon product, use product below as test:
+https://www.amazon.ca/Dell-inch-Monitor-1920-1080/dp/B096MXR34N?ref_=Oct_DLandingS_D_9557a83d_0
+Add product to some database
+Periodically update the price by repeating step 1, in a user specified time interval
+"""
+
