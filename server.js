d84dabfb522d28b8f57045037fd920f8057d8754
Joel Jacob <joeljacob500@gmail.com>
2024-08-20 10:16:44 -0400

    Sets up mongoose to create user model for database

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
-// server.js
+/*
+- Start Node Server: node server.js
+      - With nodemon(dynamic updating): npm run dev
+- Start mongod: mongod
+*/
+
+// Build express.js routes to nodejs server
 const express = require('express');  // Import the Express library
 const app = express();  // Create an Express application
 
@@ -11,3 +17,14 @@ app.get('/', (req, res) => {  // Define a route for HTTP GET requests to the roo
 app.listen(PORT, () => {  // Start the server and listen on the specified port
   console.log(`Server running on port ${PORT}`);  // Log a message when the server is running
 });
+
+// connect to mongodb
+const mongoose = require('mongoose');
+
+//NOTE: CHANGE string location to appropriate database, when we create it
+mongoose.connect('your_mongodb_connection_string', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.error(err));
