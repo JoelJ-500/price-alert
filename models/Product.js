c003da04d888fc3ad073d8089b33122332825652
Joel Jacob <joeljacob500@gmail.com>
2024-08-22 12:33:24 -0400

    Modify product schema to track time intervals for each user

--- models/Product.js
+++ models/Product.js
@@ -4,6 +4,13 @@ const ProductSchema = new mongoose.Schema({
   name: { type: String, required: true },
   originalPrice: { type: Number, required: true },
   currentPrice: { type: Number, required: true },
+  usersTracking: [
+    {
+      userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+      trackingStart: Date,
+      trackingInterval: Number, // in hours
+    }
+  ],
   priceHistory: [
     {
       timestamp: { type: Date, default: Date.now },
