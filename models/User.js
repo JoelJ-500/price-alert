8fecfe0ecb422480ff67e7f63982c691a739de23
Joel Jacob <joeljacob500@gmail.com>
2024-08-26 10:43:12 -0400

    Setting up User Authentication with JWT

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
@@ -6,6 +7,15 @@ const UserSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   updateInterval: { type: Number, default: 6 },
   trackedProducts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Product' }]
+});
+
+UserSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
 
 module.exports = mongoose.model('User', UserSchema);
