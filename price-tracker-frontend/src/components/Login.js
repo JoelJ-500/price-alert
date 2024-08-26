09d10968ab2801b2648aeecf98e588a4d4ed407c
Joel Jacob <joeljacob500@gmail.com>
2024-08-26 13:13:06 -0400

    Create basic login page

---
+++ price-tracker-frontend/src/components/Login.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import api from '../services/api';
+
+function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await api.post('/users/login', { username, password });
+      localStorage.setItem('token', res.data.token);
+    } catch (err) {
+      console.error('Login failed', err);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        placeholder="Username"
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Password"
+      />
+      <button type="submit">Login</button>
+    </form>
+  );
+}
+
+export default Login;
